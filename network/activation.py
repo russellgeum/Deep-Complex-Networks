4f04079767d4f8bae8f75361da126d59c7234aad
HC Geum <49156892+Doyosae@users.noreply.github.com>
2020-03-01 17:18:03 +0900

    Update activation.py
    modReLU update

--- network/activation.py
+++ network/activation.py
@@ -47,3 +47,49 @@ def zReLU (input1, input2):
     output2 = imagReLU
 
     return output1, output2
+
+
+def modReLU (input1, input2):
+    
+    """
+    complex_number is a+bi form
+    absolute_value is sqrt (a^2 + b^2)
+    """
+    complex_number = tf.complex(input1, input2)
+    absolute_value = tf.abs(complex_number)
+    
+    """
+    Define |z| + bias, It will be enteres to ReLU function.
+    I have question. What is porperly value for bias coefficient???
+    """
+    bias = 3 * np.ones([absolute_value.get_shape()[-1]])
+    bias = tf.Variable(bias, trainable = True, dtype = tf.float32)
+    
+    # inputs == |z| + bias
+    inputs = absolute_value + bias
+    
+    # Enter to ReLU function
+    relu_outputs = tf.keras.layers.ReLU()(inputs)
+    
+    """
+    if relu_outputs > 0
+        return 1
+    else relu_outputs <= 0 (equally "Otherwise")
+        return 0
+        
+        and all elements transformed tf.float32 type
+    """
+    flag = tf.cast(tf.cast(relu_outputs, tf.bool), tf.float32)
+    
+    
+    """
+    flag is 1 or 0
+        if flag == 1
+            value is alived
+        if flag == 9
+            valu is dead
+    """
+    output1 = input1 * flag # So, output1 maybe input1 or zero
+    output2 = input2 * flag # So output2 maybe input2 or zero
+
+    return output1, output2
